2dbc3c660317efa47dae8761a2662770270dde88
Majd <majdaj.se@gmail.com>
2019-07-03 06:18:56 +0800

    Made changes

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from builtins import print
 
 <<<<<<< HEAD
  def main(msg):
-     // Adding Git New branch: v01 (no need comments)
-     // Another comment
-     // Adding a diffrenet commit 
-     //Print a massege 
+     # Adding Git New branch: v01 (no need comments)
+     # Another comment
+     # Adding a diffrenet commit 
+     # Print a massege 
     print(msg)
 
     def secondary():
