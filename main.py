0282dc2fd6f68d34a5d95fde3771468d59af286a
Majd <majdaj.se@gmail.com>
2019-07-03 06:09:46 +0800

    new branch

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+from builtins import print
 
 <<<<<<< HEAD
  def main(msg):
+     // Adding Git New branch: v01 (no need comments)
      // Another comment
      // Adding a diffrenet commit 
      //Print a massege 
     print(msg)
+
+    def secondary():
+        print("new function")
 
 main("Hello People this is a sample")
